3ccebcfb014af5223952599ebde38b8b3ff34bf9
hantani <17hantani@gmail.com>
2024-05-22 17:48:54 +0900

    2024-05-22 study

--- src/server.js
+++ src/server.js
@@ -4,10 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("I Still love you");
 };
-app.get("/", handleHome);
+
+app.get("/", logger, handleHome);
 
 const handleLogin = (req, res) => {
   return res.send("Login");
@@ -15,6 +21,6 @@ const handleLogin = (req, res) => {
 app.get("/login", handleLogin);
 
 const handleListening = () =>
-  console.log(`Server listening on port htttp://localhost:${PORT}`);
+  console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(4000, handleListening);
